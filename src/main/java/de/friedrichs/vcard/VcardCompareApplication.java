bd8d24b9d6a4afc76c4cb80289f71cff9321aafa
Alexander Friedrichs <undso@users.noreply.github.com>
2023-08-10 21:56:05 +0200

    Initial files

---
+++ src/main/java/de/friedrichs/vcard/VcardCompareApplication.java
@@ -0,0 +1,13 @@
+package de.friedrichs.vcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcardCompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcardCompareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/friedrichs/vcard/VcardCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package de.friedrichs.vcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcardCompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
