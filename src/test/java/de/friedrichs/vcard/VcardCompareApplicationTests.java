2bf663a5779a561cd1b1c46f4582b1477d25433d
Alexander Friedrichs <friedrichs.alexander+github@gmail.com>
2023-08-28 11:51:18 +0200

    Div. Methoden zum Zusammenführen von vCards.

--- src/test/java/de/friedrichs/vcard/VcardCompareApplicationTests.java
+++ src/test/java/de/friedrichs/vcard/VcardCompareApplicationTests.java
@@ -1,13 +1,21 @@
 package de.friedrichs.vcard;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class VcardCompareApplicationTests {
 
+	@Autowired
+	private IOController ioController;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws IOException {
+		assert !ioController.getVCards().isEmpty();
+		ioController.diff();
 	}
 
 }
